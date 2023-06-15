214296faee03eae478491396a326e1aa76582531
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2023-06-15 15:22:12 +0800

    Update LeetCode 1575 统计所有可行路径.cpp

--- LeetCode 1575 统计所有可行路径.cpp
+++ LeetCode 1575 统计所有可行路径.cpp
@@ -1,19 +1,21 @@
 /*
-����һ��������ͬ���������飬�����еĵ�iλ��ʾ��i�����е�λ�ã�ͬʱ����start��finish��fuel�����ȼ�ͣ�ÿһ�ξ������ж�������ȼ�ͣ�
-������֮���ȼ������������������Ԫ��֮��Ĳ�ľ���ֵ��ͬһ�����п��Ծ�����Σ�����·������������·�������ܴܺ��������10^9+7ȡ��
+给定一个互不相同的整数数组，数组中的第i位表示第i个城市的位置，同时给定start，finish和fuel最初的燃油，每一次经过城市都会消耗燃油，
+两城市之间的燃油量就是数组中两个元素之间的差的绝对值，同一个城市可以经过多次，返回路径条数，但是路径数可能很大，最后结果对10^9+7取余
 */
 #include <iostream>
 #include <vector>
 using namespace std;
 
-//�ʼ�����������һ��dp���飬�����е�ÿһλ������start����һ�����ĵ�ȼ�ͣ��������ȼ���Ҳ����յ㣬Ȼ���ٴ���һ����������ϸ����̣�
-//ֱ��ȼ��Ϊ0���ߵ����յ㣬���������ָо�̫����ʵ�ֲ��� ��  �ⲻ����dfs�� ��
+//最开始我想的是声明一个dp数组，数组中的每一位代表着start到这一点消耗的燃油，如果还有燃油且不是终点，然后再从这一点出发继续上个过程，
+//直到燃油为0或者到达终点，不过后来又感觉太复杂实现不了 嚓  这不就是dfs吗 嚓
+
+//dfs的数据范围一般是30以内，超过了30以后就要搭配“记忆化搜索”来实现
 
 class Solution {
 public:
 	int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
 		int n = locations.size();
-		vector<vector<int>> dp(n, vector<int>(fuel + 1, -1));  // *** dp[i][fuel]��ʾ��i�㿪ʼ����ǰ����Ϊfuel������µ����յ��·����
+		vector<vector<int>> dp(n, vector<int>(fuel + 1, -1));  // *** dp[i][fuel]表示从i点开始，当前油量为fuel的情况下到达终点的路径数
 		return dfs(locations, dp, start, finish, fuel);
 	}
 
@@ -22,13 +24,13 @@ public:
 			return dp[start][fuel];
 		}
 		
-		//��һ���������û����Ŀ�ĵأ�����û����
+		//第一种情况，还没到达目的地，但是没油了
 		if (start != end && fuel == 0) {
 			dp[start][fuel] = 0;
 			return 0;
 		}
 
-		//�ڶ����������û����Ŀ�ĵأ�������ʣ������Ѿ��������������ص���
+		//第二种情况，还没到达目的地，但是所剩余的油已经不够到达其他地点了
 		int n = locations.size();
 		bool canArrive = false;
 		for (int i = 0; i < n; i++) {
@@ -44,13 +46,13 @@ public:
 			return dp[start][fuel];
 		}
 
-		//�������ܵ�·��
-		int sum = start == end ? 1 : 0;   //��Ϊ��Ŀ�����д���һ������������㹻ʱ�������յ�󻹿��Ե��������ڴ��������ٵ��յ�Ҳ��һ��·����
+		//最后计算总的路径
+		int sum = start == end ? 1 : 0;   //因为题目例子中存在一种情况，油量足够时，到达终点后还可以到其他点在从其他点再到终点也算一条路径
 		for (int i = 0; i < n; i++) {
 			if (i != start) {
 				if (fuel >= abs(locations[i] - locations[start])) {
 					sum += dfs(locations, dp, i, end, fuel - abs(locations[i] - locations[start]));
-					sum %= 1000000007;   //��һ��д����������Ϊ��һ���������ر�࣬û����int��ʾ
+					sum %= 1000000007;   //这一句写在这里是因为有一种情况结果特别多，没法用int表示
 				}
 			}
 		}
@@ -59,7 +61,7 @@ public:
 	}
 };
 
-//�������dfs�Ͷ�̬�滮���ϵ�һ���⣬��ʼ��ʱ����ֻ�뵽��dfs������д�������룬�������˹�ˮ��Ҷ�Ĵ����֪����ôд�����ǻ��ǲ�������
+//这个题是dfs和记忆化搜索相结合的一个题，开始的时候我只想到了dfs，但是写不出代码，后来看了宫水三叶的代码才知道怎么写，但是还是不熟练
 int main() {
 	vector<int> location1 = { 2,3,6,8,4 };
 	int start1 = 1, finish1 = 3, fuel1 = 5;
