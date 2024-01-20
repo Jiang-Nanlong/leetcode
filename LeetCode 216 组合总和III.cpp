b8148cd01b309a71deb76d947a25e254cf9134e1
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-01-20 22:12:36 +0800

    Add files via upload

--- LeetCode 216 组合总和III.cpp
+++ LeetCode 216 组合总和III.cpp
@@ -2,38 +2,38 @@
 #include <vector>
 using namespace std;
 
+//在1-9之间选择k个数，相加总和为n
+
 class Solution {
 public:
-	vector<vector<int>> combinationSum3(int n, int k) {
-		vector<int> cur;
+	vector<vector<int>> combinationSum3(int k, int n) {
+		vector<int> cb;
 		vector<vector<int>> res;
-		dfs(n, k, 1, 0,cur, res);
+		Helper(k, n, 1, 0, cb, res);
 		return res;
 	}
-private:
-	void dfs(int n, int k, int i,int sum, vector<int>& cur, vector<vector<int>>& res) {
-		if (cur.size() == k ) {
-			if (sum == n)
-				res.push_back(cur);
+	void Helper(int k, int n, int startindex, long long sum, vector<int>& cb, vector<vector<int>>& res) {
+		if (sum > n) return;  //剪枝操作
+
+		if (cb.size() == k) {  //这里不能把两个条件写到一起，如果写到一起，即便是当元素数量等于k时，但是sum!=n，那么程序也不会return，就会超时
+			if (sum == n) res.push_back(cb);
 			return;
 		}
-		for (int j = i; j <= 9-(k-cur.size())+1; j++) {
-			cur.push_back(j);
-			dfs(n, k, j + 1, sum + j, cur, res);
-			cur.pop_back();
+
+		for (int i = startindex; i <= 9 - (k - cb.size()) + 1; i++) {   //i<= 9 - (k - cb.size()) + 1 剪枝操作
+			cb.push_back(i);
+			Helper(k, n, i + 1, sum + i, cb, res);  //之间传参sum+i，从而避免sum回溯，只对cb进行回溯
+			cb.pop_back();
 		}
 	}
 };
 
-int main() {
-	Solution s;
-	vector<int> cur;
-	vector<vector<int>> res = s.combinationSum3(7, 3);
+void main() {
+	Solution st;
+	vector<vector<int>> res = st.combinationSum3(3, 7);
 	for (int i = 0; i < res.size(); i++) {
-		for (int j = 0; j < res[0].size(); j++) {
-			cout << " " << res[i][j];
-		}
+		for (int j = 0; j < res[0].size(); j++)
+			cout << res[i][j] << " ";
 		cout << endl;
 	}
-	return 0;
 }
--- LeetCode 77 组合.cpp
+++ LeetCode 77 组合.cpp
@@ -1,40 +1,39 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
+
+//给定两个整数n和k，返回[1,n]中所有可能的k个数的组合
 
 class Solution {
 public:
 	vector<vector<int>> combine(int n, int k) {
-		vector<int> cur;
+		vector<int> cb;
 		vector<vector<int>> res;
-		dfs(n, k, 1, cur, res);
+		Helper(n, k, 1, cb, res);
 		return res;
 	}
-
-private:
-	void dfs(int n, int k, int i, vector<int>& cur, vector<vector<int>>& res) {
-		if (cur.size() == k) {
-			res.push_back(cur);
+	void Helper(int n, int k, int startindex, vector<int>& cb, vector<vector<int>>& res) {
+		if (cb.size() == k) {
+			res.push_back(cb);
 			return;
 		}
 
-		for (int j = i; j <= n - (k - cur.size()) + 1; j++) {   //剪枝
-			cur.push_back(j);
-			dfs(n, k, j + 1, cur, res);
-			cur.pop_back();
+		for (int i = startindex; i <= n; i++) {  //这个地方可以再优化一下，
+			//比如说要找到5个数的组合，但是从startindex到数组结束已经不够五个数了，那么这一段就可以剪枝了
+			//可以把i <= n，换成i<=n-(k-cb.size())+1
+			cb.push_back(i);
+			Helper(n, k, i + 1, cb, res);
+			cb.pop_back();
 		}
 	}
 };
 
-int main() {
-	Solution s;
-	vector<vector<int>> res;
-	res = s.combine(4, 2);
+void main() {
+	Solution st;
+	vector<vector<int>> res = st.combine(4, 2);
 	for (int i = 0; i < res.size(); i++) {
-		for (int j = 0; j < res[0].size(); j++) {
-			cout << " " << res[i][j];
-		}
+		for (int j = 0; j < res[0].size(); j++)
+			cout << res[i][j] << "  ";
 		cout << endl;
 	}
-	return 0;
 }
