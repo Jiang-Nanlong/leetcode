347799c2ae49b32d9239e67ed16283e5250e608e
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-01-25 13:11:51 +0800

    Add files via upload

--- LeetCode 45 跳跃游戏II.cpp
+++ LeetCode 45 跳跃游戏II.cpp
@@ -1,31 +1,59 @@
-/*
-跳跃游戏和上一次差不多，但是这次要返回到达终点需要的最小步数
-*/
 #include <iostream>
 #include <vector>
 using namespace std;
 
+//感觉这个题真难，想不明白，跳跃游戏那题只要判断能否到达终点就行，这个题要找到到达终点最小需要几步
+//我开始想的是，如果是覆盖范围发生改变，步数就+1，但是不对。
+//感觉把这个题想成下台阶比较好想通，一个台阶就相当于当前覆盖范围所能达到的地方，同时当前台阶的不同位置也会改变下一个台阶所能到达的最远位置，
+//如果当前台阶走到头了，还不能到达终点，就只能到下一个台阶继续重复这个过程，直到终点在某一层台阶上，最后台阶数就是最小的跳跃次数。
+//但是这也是根据答案反推的，要是让我自己想，我还是想不通。
+
 class Solution {
 public:
 	int jump(vector<int>& nums) {
-		int curdistance = 0, nextdistance = 0, count = 0;
-		int m = nums.size();
-		for (int i = 0; i < m - 1; i++) {   //这里不需要遍历到最后一个点，只需要遍历到倒数第二个节点就行
-			nextdistance = max(nums[i] + i, nextdistance);
-			if (i == curdistance) {  //如果当前已经到达当前这一步可以走到的最远的范围，感觉这里有点二叉树的层序遍历的意思了
-				curdistance = nextdistance;
+		if (nums.size() == 1) return 0;
+		int curcover = 0, nextcover = 0;
+		int count = 0;
+		for (int i = 0; i <= nums.size(); i++) {
+			nextcover = max(i + nums[i], nextcover);
+			if (i == curcover) {
 				count++;
+				curcover = nextcover;
+				if (nextcover >= nums.size() - 1)
+					break;
 			}
+
+		}
+		return count;
+	}
+
+	//感觉下边的代码更好理解
+	int jump1(vector<int>& nums) {
+		if (nums.size() == 1) return 0;
+		int curcover = 0, nextcover = 0;
+		int count = 0;
+		for (int i = 0; i <= nums.size(); i++) {
+			nextcover = max(i + nums[i], nextcover);
+			if (i == curcover) {  //到达当前台阶的末尾
+				if (curcover != nums.size() - 1) {  //如果当前也不是终点
+					count++;
+					curcover = nextcover;
+					if (nextcover >= nums.size() - 1) //如果下一个台阶可以到终点
+						break;
+				}
+				else  //如果当前已经到终点了
+					break;
+			}
+
 		}
 		return count;
 	}
 };
-//感觉几天不看，现在重新回来看这个题，好像有点理解其中的意思了
+
 int main() {
-	vector<int> nums1 = { 2,3,1,1,4 };
-	vector<int> nums2 = { 2,3,0,1,4 };
-	Solution s;
-	cout << s.jump(nums1) << endl;
-	cout << s.jump(nums2) << endl;
+	Solution st;
+	vector<int> nums{ 2,3,1,1,4 };
+	int res = st.jump1(nums);
+	cout << res << endl;
 	return 0;
 }
--- LeetCode 55 跳跃游戏.cpp
+++ LeetCode 55 跳跃游戏.cpp
@@ -1,31 +1,28 @@
-/*
-给定一个非负数组，最开始从数组的第一个位置，数中的每个元素代表着从当前位置可以跳出的最大距离，判断能否跳到最后一个
-*/
 #include <iostream>
 #include <vector>
 using namespace std;
 
-//题目的重点应该放在每次移动可以跳出的范围，不必纠结每一次从哪个位置开始跳
-//嚓，贪心算法怎么这么难想，更像常识，理解的很好理解，不好理解的太难想了，嚓，每次要看解答才能看懂，本以为很复杂的代码，最后程序却很简单
-//嚓
+//数组中的每个元素表示当前位置能跳出的最大距离。
+//我记得上次做这个题就没想出来时咋用贪心的，这次还是没想出来，看的代码随想录的视频才知道。
+//感觉贪心的题好难想
+//这个题不关心每一个节点怎么跳，只关心当前能跳到的最大范围，只要在最大范围内，就能找到路径跳过去。
+//下一步跳也只是在当前的最大范围内跳跃，如果最大范围能覆盖终点，就说明可以跳到末尾
+
 class Solution {
 public:
 	bool canJump(vector<int>& nums) {
-		int count = 0;
 		if (nums.size() == 1) return true;
-		for (int i = 0; i <= count; i++) {
-			count = max(i + nums[i], count);
-			if (count >= nums.size() - 1)return true;
+		int cover = 0;
+		for (int i = 0; i <= cover; i++) {
+			cover = max(cover, i + nums[i]);
+			if (cover >= nums.size() - 1) return true;
 		}
 		return false;
 	}
 };
 
-int main() {
-	vector<int> nums1 = { 2,3,1,1,4 };
-	vector<int> nums2 = { 3,2,1,0,4 };
-	Solution s;
-	cout << s.canJump(nums1) << endl;
-	cout << s.canJump(nums2) << endl;
-	return 0;
+void main() {
+	Solution st;
+	vector<int> nums{ 2,3,1,1,4 };
+	cout << boolalpha << st.canJump(nums) << noboolalpha << endl;
 }
