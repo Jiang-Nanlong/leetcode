2c7b717b863ea75151a74adc2e4affe8c10a787a
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-02-16 19:28:22 +0800

    Add files via upload

---
+++ LeetCode 96 不同的二叉搜索树.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//这个题的dp数组以及下标的含义比较好想，但是递推公式难弄，看了代码随想录的pdf才慢慢理解，想要自己推算出来比较困难
+
+class Solution {
+public:
+	int numTrees(int n) {
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < i; j++) {
+				dp[i] += dp[j] * dp[i - j - 1];
+			}
+		}
+		return dp[n];
+	}
+};
+
+int main() {
+	Solution st;
+	cout << st.numTrees(3) << endl;
+	return 0;
+}
