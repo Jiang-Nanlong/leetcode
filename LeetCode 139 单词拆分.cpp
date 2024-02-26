07776d73b72a4145938821abec2e912e8cdb5e91
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-02-26 14:33:34 +0800

    Add files via upload

---
+++ LeetCode 139 单词拆分.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+//给定一个字符串s，和一个字典wordDict，判断字符串s能否用wordDict中的元素拼接成，wordDict中的单词可以重复使用，不要求全部都使用，能的拼接成的话返回true，否则返回false。
+//这个题是要求排列
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+		vector<bool> dp(s.size() + 1, false);
+		dp[0] = true;
+		for (int i = 1; i <= s.size(); i++) {  //遍历背包
+			for (int j = 0; j < i; j++) {  //遍历物品
+				string word = s.substr(j, i - j);
+				if (wordSet.find(word) != wordSet.end() && dp[j] == true) {  //感觉这个递推公式有点难想
+					dp[i] = true;
+				}
+			}
+		}
+		return dp[s.size()];
+	}
+};
+
+int main() {
+	Solution st;
+	string s = "applepenapple";
+	vector<string> wordDict = { "apple", "pen" };
+	cout << boolalpha << st.wordBreak(s, wordDict) << noboolalpha << endl;
+	return 0;
+}
