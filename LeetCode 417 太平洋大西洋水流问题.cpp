12123559aaa2ff0bb2a6a4e969bdba662d9ee52a
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-03-11 19:24:38 +0800

    Add files via upload

--- LeetCode 417 太平洋大西洋水流问题.cpp
+++ LeetCode 417 太平洋大西洋水流问题.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 //水只能往小于或等于当前值的地方流，题目要求是找出数组中所有点的坐标，这些点的水既可以流向太平洋，又可以流向大西洋，大西洋在右下方，太平洋在左上方
 //感觉这里的dfs需要返回值，但是如果返回bool的话，那怎么确定是到达了太平洋还是大西洋，还是都到达了
+//还是写不出来，只能看代码随想录的了他讲了一种逆流而上的方法，就是从太平洋方向和大西洋方向逆流而上，创建两个visited数组
+//最后比较两个数组的相同点，这些点就是既可以到太平洋，又可以到大西洋的点。
+//还是分为dfs和bfs两种版本
+//另外这个解法的时间复杂度是O(m*n)，因为dfs的时间复杂度是O(m*n)，然后主函数里的头两个for循环看起来是每个里边遍历了两遍dfs，时间复杂度是 n * (n * m) + m * (n * m)。
+//但是因为有visited数组的存在，每个位置只会遍历一次，所以两个for循环里的dfs(heights, visited_pacific, i, 0)和dfs(heights, visited_pacific, 0, i)可以看做一组，一共遍历一遍数组
+//dfs(heights, visited_attlantic, i, heights[0].size() - 1)和dfs(heights, visited_attlantic, heights.size() - 1, i)可以看做一组，一共遍历一遍数组，所以头两个for循环的时间复杂度其实是2*m*n。
+//最后的双层for循环的时间复杂度是m*n，所以最后总的时间复杂度是O(m*n)。
 
 class Solution {
 public:
@@ -29,10 +37,33 @@ public:
 		}
 		return res;
 	}
+
+	vector<vector<int>> pacificAtlantic1(vector<vector<int>>& heights) {
+		vector<vector<int>> res;
+		vector<vector<bool>> visited_pacific(heights.size(), vector<bool>(heights[0].size(), false));
+		vector<vector<bool>> visited_attlantic(heights.size(), vector<bool>(heights[0].size(), false));
+
+		for (int i = 0; i < heights.size(); i++) {
+			bfs(heights, visited_pacific, i, 0);
+			bfs(heights, visited_attlantic, i, heights[0].size() - 1);
+		}
+		for (int i = 0; i < heights[0].size(); i++) {
+			bfs(heights, visited_pacific, 0, i);
+			bfs(heights, visited_attlantic, heights.size() - 1, i);
+		}
+
+		for (int i = 0; i < heights.size(); i++) {
+			for (int j = 0; j < heights[0].size(); j++) {
+				if (visited_attlantic[i][j] == true && visited_pacific[i][j] == true) {
+					res.push_back({ i,j });
+				}
+			}
+		}
+		return res;
+	}
 private:
 	int dir[4][2] = { 1,0,0,1,0,-1,-1,0 };
-	//还是写不出来，只能看代码随想录的了他讲了一种逆流而上的方法，就是从太平洋方向和大西洋方向逆流而上，创建两个visited数组
-	// 最后比较两个数组的相同点，这些点就是既可以到太平洋，又可以到大西洋的点。
+
 	//int dfs(vector<vector<int>>& heights, int x, int y) {
 	//	if ((x == heights.size() - 1 && y == 0) || (x == 0 && y == heights[0].size() - 1)) return 5;
 	//	else if (x == 0 || y == 0) return 2;
@@ -57,6 +88,25 @@ private:
 				dfs(heights, visited, next_x, next_y);
 		}
 	}
+	void bfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int x, int y) {
+		queue<pair<int, int>> que;
+		que.push({ x,y });
+		visited[x][y] = true;
+		while (!que.empty()) {
+			pair<int, int> cur = que.front();
+			que.pop();
+			int cur_x = cur.first;
+			int cur_y = cur.second;
+			for (int i = 0; i < 4; i++) {
+				int next_x = cur_x + dir[i][0], next_y = cur_y + dir[i][1];
+				if (next_x < 0 || next_x >= heights.size() || next_y < 0 || next_y >= heights[0].size()) continue;
+				if (visited[next_x][next_y] == false && heights[next_x][next_y] >= heights[cur_x][cur_y]) {
+					visited[next_x][next_y] = true;
+					que.push({ next_x,next_y });
+				}
+			}
+		}
+	}
 };
 
 int main() {
@@ -75,5 +125,22 @@ int main() {
 		}
 		cout << endl;
 	}
+
+	cout << "------------------" << endl;
+	vector<vector<int>> heights1{
+		{1, 2, 2, 3, 5},
+		{3, 2, 3, 4, 4},
+		{2, 4, 5, 3, 1},
+		{6, 7, 1, 4, 5},
+		{5, 1, 1, 2, 4}
+	};
+	vector<vector<int>>res1 = st.pacificAtlantic1(heights1);
+	for (int i = 0; i < res1.size(); i++) {
+		for (int j = 0; j < res1[0].size(); j++) {
+			cout << res1[i][j] << "  ";
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
