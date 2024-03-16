8e629aecf069d96df6e5191ff1e49308a8212a4f
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-03-16 12:04:08 +0800

    Add files via upload

--- LeetCode 283 移动零.cpp
+++ LeetCode 283 移动零.cpp
@@ -16,6 +16,20 @@ public:
 		while (slow < nums.size())
 			nums[slow++] = 0;
 	}
+
+	//下边是第二次做的代码
+	void moveZeroes1(vector<int>& nums) {
+		int i = 0, j = 0;
+		for (; i < nums.size(); i++) {
+			if (nums[i] != 0) {
+				nums[j] = nums[i];
+				j++;
+			}
+		}
+		while (j < nums.size()) {
+			nums[j++] = 0;
+		}
+	}
 };
 
 void main() {
@@ -23,6 +37,11 @@ void main() {
 	vector<int> nums{ 0,1,0,3,12 };
 	st.moveZeroes(nums);
 	for (auto c : nums)
+		cout << c << " ";
+	cout << endl;
+	vector<int> nums1{ 0,1,0,3,12 };
+	st.moveZeroes1(nums1);
+	for (auto c : nums1)
 		cout << c << " ";
 	cout << endl;
 	cout << __cplusplus << endl;
