a79def57575e69407ba87a1fd96ab208fadd9253
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-03-26 12:03:39 +0800

    Add files via upload

--- LeetCode 226 翻转二叉树.cpp
+++ LeetCode 226 翻转二叉树.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <queue>
 using namespace std;
 
 struct TreeNode {
@@ -22,6 +24,53 @@ public:
 
 		return root;
 	}
+
+
+	//第二次做
+	TreeNode* invertTree1(TreeNode* root) {
+		backtrace(root);
+		return root;
+	}
+	void backtrace(TreeNode* root) {
+		if (root == nullptr) return;
+		swap(root->left, root->right);  //中
+		backtrace(root->left);  //左
+		backtrace(root->right);  //右
+	}
+
+	//上边递归还是用来前序遍历，所以也可以用前序遍历的非递归写法
+	TreeNode* invertTree2(TreeNode* root) {
+		if (root == nullptr) return root;
+		stack<TreeNode*>stk;
+		stk.push(root);
+		while (!stk.empty()) {
+			TreeNode* cur = stk.top();
+			stk.pop();
+			swap(cur->left, cur->right);
+			if (cur->right) stk.push(cur->right);
+			if (cur->left) stk.push(cur->left);
+		}
+		return root;
+	}
+
+
+	//这个题我没想到还可以用层序遍历
+	TreeNode* invertTree3(TreeNode* root) {
+        if(root==nullptr) return root;
+        queue<TreeNode*>que;
+        que.push(root);
+        while(!que.empty()){
+            int size=que.size();
+            while(size--){
+                TreeNode*cur=que.front();
+                que.pop();
+                swap(cur->left,cur->right);
+                if(cur->left) que.push(cur->left);
+                if(cur->right) que.push(cur->right);
+            }
+        }
+        return root;
+    }
 };
 
 void main() {}
