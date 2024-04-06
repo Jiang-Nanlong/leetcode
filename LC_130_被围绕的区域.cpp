4d492966a7ac4fa68f06fa0c83fe6392509d6717
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-04-06 15:39:43 +0800

    Update and rename LeetCode 130 被围绕的区域.cpp to LC_130_被围绕的区域.cpp

--- LC_130_被围绕的区域.cpp
+++ LC_130_被围绕的区域.cpp
@@ -3,10 +3,10 @@
 #include <queue>
 using namespace std;
 
-//����һ�� m x n �ľ��� board ���������ַ� 'X' �� 'O' ���ҵ����б� 'X' Χ�Ƶ����򣬲�����Щ���������е� 'O' �� 'X' ���
-//��������ȸ���һ��һģһ��������temp��Ȼ����ĸ��߿�ʼ�������������O�ͱ��X������board�Աȣ�temp��ΪO��λ��˵�����Ǳ���Χ�ĵط���Ȼ���board�е���Щ���X������
-//���Ǵ�������¼��˵��һ�ֿռ临�Ӷȸ��͵ķ��������ø������飬ֻ��Ҫ��board�����аѿ��ű߽��O��ɳ���X��O֮�����ĸ��������±������飬���һ��λ����A˵��������ͨ���߽���ʵ�O�������޸ĳ�O���С�
-// ���һ��λ����O˵�����Ǳ���Χ�ģ��޸ĳ�X���С�
+//给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充
+//我想的是先复制一个一模一样的数组temp，然后从四个边框开始遍历，如果遇到O就变成X，最后和board对比，temp中为O的位置说明就是被包围的地方，然后把board中的这些变成X就行了
+//但是代码随想录里说了一种空间复杂度更低的方法，不用复制数组，只需要在board数组中把靠着边界的O变成除了X和O之外的字母，最后重新遍历数组，如果一个位置是A说明它可以通过边界访问的O，重新修改成O就行。
+// 如果一个位置是O说明就是被包围的，修改成X就行。
 class Solution {
 public:
 	void solve(vector<vector<char>>& board) {
@@ -38,13 +38,13 @@ public:
 		}
 		for (int i = 0; i < board.size(); i++) {
 			for (int j = 0; j < board[0].size(); j++) {
-				if (board[i][j] == 'O') board[i][j] = 'X';  //ע��������if��˳��Ҫ���
+				if (board[i][j] == 'O') board[i][j] = 'X';  //注意这两个if的顺序不要搞混
 				if (board[i][j] == 'A') board[i][j] = 'O';
 			}
 		}
 	}
 
-	//bfs�汾
+	//bfs版本
 	void solve2(vector<vector<char>>& board) {
 		for (int i = 0; i < board.size(); i++) {
 			if (board[i][0] == 'O') bfs(board, i, 0);
@@ -56,7 +56,7 @@ public:
 		}
 		for (int i = 0; i < board.size(); i++) {
 			for (int j = 0; j < board[0].size(); j++) {
-				if (board[i][j] == 'O') board[i][j] = 'X';  //ע��������if��˳��Ҫ���
+				if (board[i][j] == 'O') board[i][j] = 'X';  //注意这两个if的顺序不要搞混
 				if (board[i][j] == 'A') board[i][j] = 'O';
 			}
 		}
@@ -78,7 +78,7 @@ private:
 
 
 	void dfs1(vector<vector<char>>& board, int x, int y) {
-		board[x][y] = 'A';  //�ѿ���ͨ���߽���ʵ�O���޸ĳ�A
+		board[x][y] = 'A';  //把可以通过边界访问的O都修改成A
 		for (int i = 0; i < 4; i++) {
 			int next_x = x + dir[i][0];
 			int next_y = y + dir[i][1];
