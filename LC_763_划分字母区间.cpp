1b73b743bda68fefc0c8f7c0004572a7e249aae7
江南龙 <64135716+Jiang-Nanlong@users.noreply.github.com>
2024-04-08 14:24:55 +0800

    Update and rename LeetCode 763 划分字母区间.cpp to LC_763_划分字母区间.cpp

--- LC_763_划分字母区间.cpp
+++ LC_763_划分字母区间.cpp
@@ -3,14 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-//ģ����������ص���������⣬�ȼ����ÿ����ĸ�Ŀ�ʼ�ͽ������䣬Ȼ���ٺ���������һ����������в�����
-//��������������s=vhaagbqkaqʱ���д�����ȡ�����[1,1,8]��ʵ�ʽ����[2,7]������ط���֪��զ���£��ȹ������ٿ�
+//模仿射箭和无重叠区间的问题，先计算出每个字母的开始和结束区间，然后再和这两个题一样对区间进行操作。
+//但是这种在遇到s=vhaagbqkaq时会有错误，争取结果是[1,1,8]，实际结果是[2,7]，这个地方不知道咋回事，等过完年再看
 class Solution {
 public:
 	static bool cmp(vector<int>& a, vector<int>& b) {
 		return a[0] < b[0];
 	}
-	vector<vector<int>> getcount(string s) {   //ģ������ĺ����ص���������⣬��ȡÿ����ĸ������
+	vector<vector<int>> getcount(string s) {   //模仿射箭的和无重叠子区间的题，获取每个字母的区间
 		vector<vector<int>> hash(26, vector<int>(2, INT_MIN));
 		vector<vector<int>> res;
 		for (int i = 0; i < s.size(); i++) {
@@ -52,21 +52,21 @@ int main() {
 	return 0;
 }
 
-//���Ǵ�������¼��pdf�����Ĵ��룬���ҵ�һ��������⿴������101�Ĵ���һģһ�����о����˼·�ر����ֻ��עÿ����ĸ�����λ�ã�
-//�����Ŀ�ʼλ�á�
+//这是代码随想录的pdf给出的代码，和我第一次做这个题看的力扣101的代码一模一样，感觉这个思路特别巧妙，只关注每个字母的最后位置，
+//不关心开始位置。
 /*
 class Solution {
 public:
 	vector<int> partitionLabels(string s) {
 		int count[27] = { 0 };
-		for (int i = 0; i < s.size(); i++) {  //ͳ��ÿ����ĸ���ֵ����λ��
+		for (int i = 0; i < s.size(); i++) {  //统计每个字母出现的最后位置
 			count[s[i] - 'a'] = i;
 		}
 		int left = 0, right = 0;
 		vector<int> res;
 		for (int i = 0; i < s.size(); i++) {
 			right = max(right, count[s[i] - 'a']);
-			if (i == right) {  //����ж������е�����
+			if (i == right) {  //这个判断条件有点巧妙
 				res.push_back(right - left + 1);
 				left = right + 1;
 			}
@@ -76,17 +76,17 @@ public:
 };
 */
 
-//��������֮ǰд��
+//以下是我之前写的
 /*
-����һ���ַ�����Ҫ���ַ������ֳɾ������Ƭ�Σ�ͬһ��ĸ���ֻ�ܳ�����ͬһƬ���У����ֽ����Ҫ����Ҫ�󣺰���ЩƬ�ΰ�˳�����ӻ��������ԭ�����ַ���
-����һ���б����б���ÿһ��Ԫ�ر�ʾһ��Ƭ�εĳ��ȡ�
-ע���ַ���ȫ��Сд��ĸ��ɣ�����1<= s.length <= 500
+给定一个字符串，要把字符串划分成尽量多的片段，同一字母最多只能出现在同一片段中，划分结果需要满足要求：把这些片段按顺序链接还可以组成原来的字符串
+返回一个列表，列表中每一个元素表示一个片段的长度。
+注：字符串全由小写字母组成，长度1<= s.length <= 500
 */
 
 
-//���û��˼·������LeetCode101�ϵ���ʾ���ֿ��Լ�¼ÿ����ĸ���ֵ��ʼλ�ú����λ�ã������ͱ�����������ˣ������ת��Ϊ����м������໥����������
-//���˼��������»�����������ҾͶ��ˣ��ȱ���һ��string��ͳ��ÿ���ַ����ֵ����λ�ã�Ȼ���ͷ��ʼ������ͳ�Ƶ�ǰ���������ַ����������λ�ã������Ļ���
-//����������������λ��ʱ��˵��ǰ�߼����������ַ����ѵ�������λ�ã������������ֹλ�õ��ַ�Ҳ�Ѿ����������λ���ˣ�������һ���ַ����������ˣ����ԾͿ��Ի���һ���ˡ�
+//起初没有思路，看了LeetCode101上的提示后发现可以记录每种字母出现的最开始位置和最后位置，这样就变成区间问题了，问题就转化为求解有几部分相互独立的区间
+//过了几天再重新回来看这个题我就懂了，先遍历一遍string，统计每个字符出现的最后位置，然后从头开始遍历，统计当前遍历过的字符的最大的最后位置，这样的话，
+//当到达这个最大的最后位置时，说明前边几个其他的字符早已到达最后的位置，以至于最大终止位置的字符也已经遍历到最后位置了，所以这一段字符都遍历完了，所以就可以划分一下了。
 /*
 class Solution {
 public:
@@ -96,7 +96,7 @@ public:
 			count[s[i] - 'a'] = i;
 		}
 		vector<int> result;
-		int left = 0, right = 0;  //������¼ÿһ����������Ҷ˵㣬Ȼ��������䳤��
+		int left = 0, right = 0;  //用来记录每一段区间的左右端点，然后计算区间长度
 		for (int i = 0; i < s.size(); i++) {
 			right = max(right, count[s[i] - 'a']);
 			if (i == right) {
